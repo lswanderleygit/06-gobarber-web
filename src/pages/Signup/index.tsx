ce2b72b6b49f5acff4f5802056ef31cf423da1ba
Lucas Wanderley <lucaswanderley4@gmail.com>
2020-12-22 11:40:30 -0800

    feat: show errors to input form

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useRef } from 'react'
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
+
+import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -11,8 +14,12 @@ import Button from '../../components/Button'
 import { Container, Content, Background } from './styles'
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
+
   const handlerSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({})
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -25,7 +32,7 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       })
     } catch (err) {
-      console.log({ err })
+      formRef.current?.setErrors(getValidationErrors(err))
     }
   }, [])
 
@@ -35,7 +42,7 @@ const SignUp: React.FC = () => {
 
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <Form onSubmit={handlerSubmit}>
+        <Form ref={formRef} onSubmit={handlerSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
